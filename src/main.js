0ab0765efb32f6017d607513a5e06a1dba7ca7d9
Patrik Sedmina <patrik@sedmina.com>
2020-08-30 09:47:27 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
-import Vue from 'vue';
-import firebase from 'firebase';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import firebase from 'firebase'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-let app = '';
+let app = ''
 const config = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
   databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
   projectId: 'YOUR_PROJECT_ID',
   storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SEND_ID'
-};
+  messagingSenderId: 'YOUR_MESSAGING_SEND_ID',
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
     /* eslint-disable no-new */
     app = new Vue({
       router,
-      render: h => h(App)
-    }).$mount('#app');
+      render: (h) => h(App),
+    }).$mount('#app')
   }
-});
+})
